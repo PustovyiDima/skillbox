6e102c77590b3846c65fcb384fb08ae87eeb8355
Abbadis <pustovoydima@gmail.com>
2020-08-29 22:54:50 +0300

    add php backend

--- close.js
+++ close.js
@@ -8,4 +8,21 @@ $(function(){
 		$('.form__layout').css('display','none');
 	});
 
+
+	$('.order__form button').on('click',function(){
+		let formData = $('.order__form ').serializeArray();
+		let request = {};
+		for (i in formData) {
+			request[formData[i].name] = formData[i].value;
+		}
+		
+		$.post('./api/api.php', request, function(response){
+			if (response == "1"){
+				$('.form__layout').css('display','none');
+			};
+		});
+
+		return false;
+	});
+
 });
